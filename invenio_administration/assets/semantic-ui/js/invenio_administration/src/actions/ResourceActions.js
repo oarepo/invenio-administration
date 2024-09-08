968c1283b73f42a8516a71e068f0c5825ed36299
Mirek Simek <miroslav.simek@gmail.com>
2024-09-08 12:17:50 +0200

    adding a single change

--- invenio_administration/assets/semantic-ui/js/invenio_administration/src/actions/ResourceActions.js
+++ invenio_administration/assets/semantic-ui/js/invenio_administration/src/actions/ResourceActions.js
@@ -1,15 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 import { Button, Modal, Icon } from "semantic-ui-react";
 import { ActionForm } from "../formik";
 import ActionModal from "./ActionModal";
+import _get from "lodash/get";
 import _isEmpty from "lodash/isEmpty";
 import Overridable from "react-overridable";
+import { InvenioAdministrationActionsApi } from "../api";
 
 class ResourceActions extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading: false,
       modalOpen: false,
       modalHeader: undefined,
       modalBody: undefined,
@@ -44,6 +47,42 @@ class ResourceActions extends Component {
     });
   };
 
+  onActionSubmit = async (e, { dataActionKey, ...rest }) => {
+    // TODO: Taken from ActionForm - needs refactoring/merge
+    console.log({ rest });
+    this.setState({ loading: true });
+    const actionEndpoint = this.getEndpoint(dataActionKey);
+
+    try {
+      const response = await InvenioAdministrationActionsApi.resourceAction(
+        actionEndpoint
+      );
+      // this.setState({ loading: false, success: response.data });
+      this.onModalClose(response.data);
+    } catch (e) {
+      console.error(e);
+      this.setState({ loading: false });
+    }
+  };
+
+  getEndpoint = (actionKey) => {
+    // TODO: Taken from ActionForm - needs refactoring/merge
+    const { resource } = this.props;
+    let endpoint;
+    // get the action endpoint from the current resource links
+    endpoint = _get(resource, `links.actions[${actionKey}]`);
+
+    // endpoint can be also within links, not links.action
+    // TODO: handle it in a nicer way
+    if (_isEmpty(endpoint)) {
+      endpoint = _get(resource, `links[${actionKey}]`);
+    }
+    if (!endpoint) {
+      console.error("Action endpoint not found in the resource!");
+    }
+    return endpoint;
+  };
+
   closeModal = () => {
     this.setState({
       modalOpen: false,
@@ -64,7 +103,7 @@ class ResourceActions extends Component {
 
   render() {
     const { actions, Element, resource } = this.props;
-    const { modalOpen, modalHeader, modalBody } = this.state;
+    const { modalOpen, modalHeader, modalBody, loading } = this.state;
     return (
       <>
         {Object.entries(actions).map(([actionKey, actionConfig]) => {
@@ -73,13 +112,19 @@ class ResourceActions extends Component {
           return (
             <Element
               key={actionKey}
-              onClick={this.onModalTriggerClick}
+              onClick={
+                !actionConfig.payload_schema
+                  ? this.onModalTriggerClick
+                  : this.onActionSubmit
+              }
               payloadSchema={actionConfig.payload_schema}
               dataName={actionConfig.text}
               dataActionKey={actionKey}
               basic
               icon={!_isEmpty(icon)}
               labelPosition={labelPos}
+              loading={loading}
+              disabled={loading}
             >
               {!_isEmpty(icon) && <Icon name={icon} />}
               {actionConfig.text}...
